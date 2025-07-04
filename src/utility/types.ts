b7482de150610216a18f4443be20e10cab084b51
OsamaFahim <osamafahim654@gmail.com>
2025-07-04 17:32:54 +0500

    Fixed Comments in type.ts utility file

--- src/utility/types.ts
+++ src/utility/types.ts
@@ -1,6 +1,6 @@
 //Creating an interface to map the input form data, have chosen 6 fields, 
-// First name, surname, email address, mobile number, distributor, product provider, service provider, stock handler
-//After creating the interface, I will be uiing it in my state to manage the form Data so that i dont have to use multiple stooks
+// First name, surname, email address, mobile number, distributor, product provider After creating the interface, I will 
+// be uiing it in my state to manage the form Data so that i dont have to use multiple states
 //for all the fields
 export interface FormData {
     firstName: string;
